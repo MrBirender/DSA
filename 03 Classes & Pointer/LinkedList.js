292f28074a3c356a1b47d51b7ce95d4e7b287c6d
MrBirender <birenderyadav0918@gmail.com>
2025-07-07 12:41:42 +0530

    linkedlist

--- 03 Classes & Pointer/LinkedList.js
+++ 03 Classes & Pointer/LinkedList.js
@@ -1,36 +1,61 @@
 class Node {
-    constructor(value){
-        this.value = value
-        this.next = null
-    }
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
 }
 
 class LinkedList {
-    constructor(value){
-        const newNode = new Node(value)
-        this.head = newNode
-        this.tail = this.head
-        this.length = 1
+  constructor(value) {
+    const newNode = new Node(value);
+    this.head = newNode;
+    this.tail = newNode;
+    this.length = 1;
+  }
+
+  push(value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  pop() {
+    if(!this.head) return undefined
+    let temp = this.head;
+    let pre = this.head;
+    while (temp.next) {
+      pre = temp;
+      temp = temp.next;
     }
 
-    push(value){
-        const newNode = new Node(value)
-        if(!this.head){
-            this.head = newNode
-            this.tail = newNode
-        }else{
-            this.tail.next = newNode
-            this.tail = newNode
-        }
-        this.length++
-        return this
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = pre;
+      pre.next = null;
     }
+    this.length--;
+    return temp
+  }
 }
 
-const linkListOne  = new LinkedList(9)
-linkListOne.push(7)
-linkListOne.push(8)
-linkListOne.push(5)
-console.dir(linkListOne, { depth: null });
-
-// console.log(linkListOne)
+const newList = new LinkedList(1);
+newList.push(2);
+newList.push(3);
+newList.push(4);
+console.log(newList.pop());
+console.dir(newList, { depth: null });
+console.log(newList.pop());
+console.dir(newList, { depth: null });
+console.log(newList.pop());
+console.dir(newList, { depth: null });
+console.log(newList.pop());
+console.dir(newList, { depth: null });
