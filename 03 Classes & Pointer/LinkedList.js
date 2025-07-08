a0880033d1886d451d54b29692ab8f6e659b2640
MrBirender <birenderyadav0918@gmail.com>
2025-07-08 11:07:28 +0530

    pop shift unshift get

--- 03 Classes & Pointer/LinkedList.js
+++ 03 Classes & Pointer/LinkedList.js
@@ -27,7 +27,7 @@ class LinkedList {
   }
 
   pop() {
-    if(!this.head) return undefined
+    if (!this.head) return undefined;
     let temp = this.head;
     let pre = this.head;
     while (temp.next) {
@@ -43,7 +43,46 @@ class LinkedList {
       pre.next = null;
     }
     this.length--;
+    return temp;
+  }
+
+  unshift(value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  shift() {
+    let temp = this.head;
+    if (!this.head) return undefined;
+
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.head = temp.next;
+      temp.next = null;
+    }
+    this.length--;
+    return temp;
+  }
+
+  get(index){
+    if(index < 0 || index >= this.length) return undefined
+
+    let temp = this.head
+    for(let i=0; i<index; i++){
+        temp = temp.next
+    }
     return temp
+
   }
 }
 
@@ -51,11 +90,10 @@ const newList = new LinkedList(1);
 newList.push(2);
 newList.push(3);
 newList.push(4);
-console.log(newList.pop());
-console.dir(newList, { depth: null });
-console.log(newList.pop());
-console.dir(newList, { depth: null });
-console.log(newList.pop());
-console.dir(newList, { depth: null });
-console.log(newList.pop());
-console.dir(newList, { depth: null });
+// newList.pop();
+// console.log(newList.pop());
+// newList.unshift(5);
+// console.log(newList.shift());
+console.log(newList.get(2))
+
+// console.dir(newList, { depth: null });
